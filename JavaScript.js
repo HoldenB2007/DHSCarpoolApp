4fb1e7eee5ea24d92e0268fc61e6b9e0b7f43b98
holden.bronson07 <holdenbronson@gmail.com>
2024-02-07 11:35:25 -0700

    Started sign in sign up feature. Still bugs, needs fixing. currently signup function isn't running for some reason.

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,68 @@
-signout.addEventListener("click", signout);
+var currentUserIndex = -1;
+var allUsersPasswords = [];
+var allUsersGender = [];
+var allUsersParentEmail = [];
+var allUsersEmail = [];
+var studentNumbers = [123]
+
 function signout () {
     window.location.replace("./Sign-in.html");
+    currentUserIndex = -1;
 }
+
+function signIn () {
+    window.location.replace("./home.html");
+    var possibleUserEmail = document.getElementById("email-input-sign-in").value;
+    var possiblePassword = document.getElementById("password-input-sign-in").value;
+    var possibleUserIndex = -1;
+    var validUserEmail = false;
+    var validCorrespondingPassword = false;
+    for (var a = 0; a < allUsersEmail.length; a++) {
+        if (possibleUserEmail === allUsersEmail[a]) {
+            validUserEmail = true;
+            possibleUserIndex = a;
+            if (possiblePassword === allUsersPasswords[a]) {
+                validCorrespondingPassword = true;
+            }
+        }
+    }
+        if ((validUserEmail === true) && (validCorrespondingPassword === false)) {
+            currentUserIndex = possibleUserIndex;
+            window.location.replace("./home.html");
+        }
+}
+
+function SignUpFunction () {
+    window.location.replace("./home.html");
+    var newUserEmail = document.getElementById("email-input-sign-up").value;
+    var newUserPassword = document.getElementById("password-input-sign-up").value;
+    var newUserStudentNum = document.getElementById("sign-up-student-number").value;
+    var newUserParentEmail = document.getElementById("sign-up-parent-email").value;
+    var newUserGender = document.getElementById("Birth-Assigned-Sex").value;
+    var validStuNum = false;
+    var previousUser = false;
+
+    for (var i = 0; i < allUsersEmail.length; i++) {
+        if (newUserEmail === allUsersEmail[i]) {
+            previousUser = true;
+            alert("This email already has an account");
+        }
+    }
+    for (var p = 0; p < studentNumbers.length; p++) {
+        if (newUserStudentNum === studentNumbers[p]) {
+            validStuNum = true;
+        } else {
+            alert("Invalid Student Number");
+        }
+    }
+    if ((validStuNum === true) && (previousUser === false)) {
+        allUsersPasswords.push(newUserPassword);
+        allUsersGender.push(newUserGender);
+        allUsersParentEmail.push(newUserParentEmail);
+        allUsersEmail.push(newUserEmail);
+
+        window.location.replace("./home.html");
+        currentUserIndex = allUsersEmail.length - 1;
+    }
+}
+
