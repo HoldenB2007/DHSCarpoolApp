09733891a62b08aa902b9764874d71a865651de6
holden.bronson07 <holdenbronson@gmail.com>
2024-03-06 11:34:59 -0700

    Finished SignIn encryption and functionality. Now working on session management.

--- currentServer.js
+++ currentServer.js
@@ -5,11 +5,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt')
 
+const session = require('express-session')
+
 app.use(express.static('Static'));
-// app.get('/', (req, res) => {
-//
-//     res.sendFile(path.join(__dirname, './home.html'));
-// })
 
 app.listen(port, () => {
     console.log('Server started at http://localhost:' + port + '/Sign-in.html');
@@ -24,74 +22,77 @@ var allUsersEmail = ["admin@gmail.com"];
 var studentNumbers = ["1", '2', '3'];
 
 app.post('/signIn', (req, res) => {
-    console.log(req.body)
-    var data = req.body;
+
+    const data = req.body;
     //Sign In
-    var possibleUserEmail = data.email;
-    var possiblePassword = data.password;
-    var validUserEmail = false;
+    var possibleUserEmail = data.emailSignIn;
+    const possiblePassword = data.passwordSignIn;
     var validCorrespondingPassword = false;
     for (var a = 0; a < allUsersEmail.length; a++) {
         if (possibleUserEmail === allUsersEmail[a]) {
-            validUserEmail = true;
-            if (possiblePassword === allUsersPasswords[a]) {
-                validCorrespondingPassword = true;
-            }
+            console.log('evaluating password');
+            bcrypt
+                .compare(possiblePassword, allUsersPasswords[a], function (err, result) {
+                    if (result) {
+                        console.log('signed in!');
+                        //session management
+                        res.redirect('/home.html')
+                    }
+                });
         }
-    }
-    if ((validUserEmail === true) && (validCorrespondingPassword === false)) {
-        console.log('signed in!');
-        res.redirect('/home.html')
     }
 })
 
-    app.post('/signUp', (req, res) => {
-        console.log(req.body)
-        var data = req.body;
-        //Sign Up
-        var newUserEmail = data.email;
-        var newUserPassword = data.password;
-        var newUserStudentNum = data.studentNumber;
-        var newUserParentEmail = data.parentEmail;
-        var newUserGender = data.gender;
-        var validStuNum = false;
-        var previousUser = false;
+        app.post('/signUp', (req, res) => {
 
-        for (var i = 0; i < allUsersEmail.length; i++) {
-            if (newUserEmail === allUsersEmail[i]) {
-                previousUser = true;
-                res.send('Email already in use');
+            var data = req.body;
+            //Sign Up
+            var newUserEmail = data.email;
+            var newUserPassword = data.password;
+            var newUserStudentNum = data.studentNumber;
+            var newUserParentEmail = data.parentEmail;
+            var newUserGender = data.gender;
+            var validStuNum = false;
+            var previousUser = false;
+
+            for (var i = 0; i < allUsersEmail.length; i++) {
+                if (newUserEmail === allUsersEmail[i]) {
+                    previousUser = true;
+                    res.send('Email already in use');
+                }
             }
-        }
-        for (var p = 0; p < studentNumbers.length; p++) {
-            if (newUserStudentNum === studentNumbers[p]) {
-                validStuNum = true;
+            for (var p = 0; p < studentNumbers.length; p++) {
+                if (newUserStudentNum === studentNumbers[p]) {
+                    validStuNum = true;
+                }
             }
-        }
-        if (validStuNum === false) {
-            res.send('Invalid Student Number');
-        }
-        if ((validStuNum === true) && (previousUser === false)) {
-            allUsersPasswords.push(newUserPassword);
-            allUsersGender.push(newUserGender);
-            allUsersParentEmail.push(newUserParentEmail);
-            allUsersEmail.push(newUserEmail);
-            //window.location.assign('./home.html');
+            if (validStuNum === false) {
+                res.send('Invalid Student Number');
+            }
+            if ((validStuNum === true) && (previousUser === false)) {
+                allUsersGender.push(newUserGender);
+                allUsersParentEmail.push(newUserParentEmail);
+                allUsersEmail.push(newUserEmail);
+                //window.location.assign('./home.html');
 
-            const plaintextPassword = newUserPassword;
+                const plaintextPassword = newUserPassword;
 
-// Generate a salt (a random string) to add complexity to the hashing process
-            const saltRounds = 10
-            const salt = bcrypt.genSaltSync(saltRounds)
+                // Generate a salt (a random string) to add complexity to the hashing process
+                const saltRounds = 10
+                const salt = bcrypt.genSaltSync(saltRounds)
 
-// Hash the password using the generated salt
-            const hashedPassword = bcrypt.hashSync(plaintextPassword, salt)
-
-            console.log('Hashed Password:', hashedPassword)
-            res.redirect('/home.html');
-            /*console.log(allUsersPasswords);
-            console.log(allUsersGender);
-            console.log(allUsersParentEmail);
-            console.log(allUsersEmail)*/
-        }
-    })
+                // Hash the password using the generated salt
+                const hashedPassword = bcrypt.hashSync(plaintextPassword, salt)
+                allUsersPasswords.push(hashedPassword);
+                //session management
+                req.session.save (() => {
+                    req.session.logged_in = true;
+                    req.session.user = {
+                        email: newUserEmail,
+                        gender: newUserGender,
+                        parentEmail: newUserParentEmail,
+                    };
+                });
+                res.redirect('/home.html');
+            }
+        })
