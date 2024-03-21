9ee754aa3ee9d086c453dd91be8f64ce87102da9
holden.bronson07 <holdenbronson@gmail.com>
2024-03-21 00:02:06 -0600

    Got session management working with sign-in and sign out. I'm only really able to do stuff in console.log though

--- Static/JavaScript.js
+++ Static/JavaScript.js
@@ -1,4 +1,48 @@
 function signout () {
+    const data = { key: 'value' };
+
+    // Options for the Fetch API
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    // URL where the POST request should be sent
+    const url = '/logout';
+    // Sending the POST request
+    fetch(url, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('POST request succeeded with JSON response:', data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the POST request:', error);
+        });
     window.location.replace("./Sign-in.html");
 }
 
+document.getElementById("DOMContentLoaded", function(event) {
+    console.log("html files have been loaded")
+});
+/*
+document.getElementById("homeLink").addEventListener('click', function() {
+    console.log('triggered');
+    fetch('/api/data')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
+ */
